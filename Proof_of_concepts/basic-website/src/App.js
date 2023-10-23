c0d566e31a49adb3a1494c85058d8a69b967a759
danielligory <daniel.ligory@gmail.com>
2023-10-23 17:47:54 +0100

    feat: basic concept of blog page
    The basic format of the blog page is complete.
    
    Next step would be the CSS

--- Proof_of_concepts/basic-website/src/App.js
+++ Proof_of_concepts/basic-website/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header className="App-header">
+          RHUL Blog Post
+        <nav>
+          <ul>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/features'>Features</a></li>
+            <li><a href='/galleries'>Galleries</a></li>
+            <li><a href='/contact'>Contact Us</a></li>
+          </ul>
+        </nav>
       </header>
+
+      <main>
+        <section ClassName = "blog-post">
+          <h2> Blog Post Title </h2>
+          <p> Published by John Wick on Janurary 15, 2023 </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+            feugiat massa. Praesent sed bibendum odio. Aenean facilisis quam sit
+            amet diam scelerisque, ac hendrerit lorem pharetra.
+          </p>
+          <a href='/post/1'> Read More </a>
+        </section>
+
+        <section ClassName = "blog-post">
+          <h2>Another Blog Post</h2>
+          <p> Published by Rick Roll on Feburary 02, 2023 </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+            feugiat massa. Praesent sed bibendum odio. Aenean facilisis quam sit
+            amet diam scelerisque, ac hendrerit lorem pharetra.
+          </p>
+          <a href='/post/2'> Read More </a>
+        </section>
+      </main>
+
+      <footer>
+        <p>&copy; 2023 RHUL Blog </p>
+        <div className = "social-icons">
+          <a href='https://www.facebook.com/royalholloway/' className = 'social-icons'>Facebook</a>
+          <a href='https://twitter.com/RoyalHolloway/' className = 'social-icons'>Twitter</a>
+          <a href='https://www.instagram.com/royalholloway/' className = 'social-icons'>Instagram</a>
+        </div>
+      </footer>
     </div>
   );
 }
