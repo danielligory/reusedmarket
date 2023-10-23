364d6607ca353f25a192fa1d4d93bb966d488e1a
Daniel Ligory <daniel.ligory@gmail.com>
2023-10-23 12:03:42 +0100

    feat: Practice TodoList React
    This is a TodoList made using react. This was done following a tutorial
    
    https://www.youtube.com/watch?v=Rh3tobg7hEo&t=1849s

---
+++ practiceCode/React/react-todo-list/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
+import "./styles.css"
+
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, 
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+    </>
+  )
+}
