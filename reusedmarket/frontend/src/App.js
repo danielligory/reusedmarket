fea40e27622ab84a36ed64af3b17c27e25be2656
Daniel <daniel.ligory@gmail.com>
2023-11-07 10:51:19 +0000

    feat: adds Header and ProductList js files
    Frontend Header and ProductList files added

--- reusedmarket/frontend/src/App.js
+++ reusedmarket/frontend/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <ProductList />
     </div>
   );
 }
