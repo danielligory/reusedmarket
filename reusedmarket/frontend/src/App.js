edb8cbdbb9c4d1ef4bdec3e2f2ec8c02d0727a80
danielligory <daniel.ligory@gmail.com>
2023-11-19 01:11:39 +0000

    Feat: switching over to mongoDB server to render list
    Using the backend to display the product cards.
    
    ALMOST WORKING, NO ERRORS JUST NOT DISPLAYING

--- reusedmarket/frontend/src/App.js
+++ reusedmarket/frontend/src/App.js
@@ -2,6 +2,7 @@ import './styles/App.css';
 import NavBar from './components/NavBar';
 import ProductList from './components/ProductList';
 
+
 function App() {
   return (
     <div className="App">
