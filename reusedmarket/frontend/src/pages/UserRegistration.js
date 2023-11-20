e74a1f388cd83ed8aadf471d3ae450bd34f08917
Daniel <daniel.ligory@gmail.com>
2023-11-20 15:33:15 +0000

    feat: updated UserRegistration
    updated the userRegistration

--- reusedmarket/frontend/src/pages/UserRegistration.js
+++ reusedmarket/frontend/src/pages/UserRegistration.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axois';
+import axios from 'axios';
 
 const UserRegistration = () => {
     const [formData, setFormData] = useState({
@@ -10,6 +10,46 @@ const UserRegistration = () => {
 
 
     const handleChange = (e) => {
-        
-    }
-}
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('../../../backend/routes/userRoutes', formData);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error.response.data);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type = 'text'
+                name = 'username'
+                placeholder = 'Username'
+                value = {formData.username}
+                onChange = {handleChange}
+            />
+            <input 
+                type = 'email'
+                name = 'email'
+                placeholder = 'Email'
+                value = {formData.email}
+                onChange = {handleChange}
+            />
+            <input 
+                type = 'password'
+                name = 'password'
+                placeholder = 'Password'
+                value = {formData.password}
+                onChange = {handleChange}
+            />
+            <button type='submit'>Register</button>
+        </form>
+    );
+};
+
+export default UserRegistration;
