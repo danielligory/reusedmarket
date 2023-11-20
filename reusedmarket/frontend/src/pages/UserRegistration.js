e87aaa59045f85f4d92765b4aa8bc11d5931c820
danielligory <daniel.ligory@gmail.com>
2023-11-20 13:04:28 +0000

    Feat: added registration page
    Registration page is where the user will be able to register their account

---
+++ reusedmarket/frontend/src/pages/UserRegistration.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import axios from 'axois';
+
+const UserRegistration = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+
+    const handleChange = (e) => {
+        
+    }
+}
